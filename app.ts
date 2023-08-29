7fd1c02c8242dcc447a1b307ea11182174f812ab
abhishek9373 <abhishekgund500@gmail.com>
2023-08-29 11:12:07 +0530

    tmp config

--- app.ts
+++ app.ts
@@ -16,7 +16,7 @@ const server = new http.Server(expressApp);
 export const io = new Server(server, {
     cors: {
         origin: "*",
-        // methods: ["GET", "POST", "PATCH", "PUT", "DELETE"]
+        methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "HEAD"]
     }
 });
 
