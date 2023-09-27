e5fbdaaccc2e12b785e328d4a1891a9a2bd74384
= <=>
2023-09-27 23:28:09 +0530

    user verified default --> true

--- modules/user/models/User.ts
+++ modules/user/models/User.ts
@@ -16,7 +16,7 @@ export const userSchema: Mongoose.Schema = new Mongoose.Schema({
     },
     isVerified: {
         type: Boolean,
-        default: false
+        default: true
     },
     stars: {
         type: Number,
